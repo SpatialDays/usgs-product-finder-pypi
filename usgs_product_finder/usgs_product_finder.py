cbf9a89d43d2663eb1503052c272a89a204d0607
Ivica Matic <ivica.matic@spatialdays.com>
2023-10-18 12:22:28 +0100

    docs: tidy-up docstrings

--- usgs_product_finder/usgs_product_finder.py
+++ usgs_product_finder/usgs_product_finder.py
@@ -18,12 +18,12 @@ class UsgsProductFinder:
         """
         Initialize the UsgsProductFinder class.
 
-        If the path_for_usgs_data parameter is filled, the library will download csv's and auxiliary files to that path.
-        If the files already exist, they will not be downloaded again unless they are older than one day or the
+        If the path_for_usgs_data parameter is filled, the library will obtain csv's and auxiliary files to that path.
+        If the files already exist, they will not be obtained again unless they are older than one day or the
         max_age_of_usgs_data parameter specified.
 
-        :param path_for_usgs_data:
-        :param max_age_of_usgs_data:
+        :param path_for_usgs_data: Path where the library will obtain csv's and auxiliary files
+        :param max_age_of_usgs_data: Maximum age of the obtained files in days
         """
         logger.debug("Initializing UsgsProductFinder")
         self.max_age_of_usgs_data = max_age_of_usgs_data
@@ -41,15 +41,17 @@ class UsgsProductFinder:
         Find products that intersect with a geodataframe.
         :param filtered_geodataframe: AOI-Filtered geodataframe
         :param satellite: Number for the satellite
-        :return:
+        :param minimal_output: If True, returns a list of dicts with display_id, ordering_id and landsat_product_id,
+         if False, returns a pandas dataframe from usgs csv
+        :return: List of dicts or pandas dataframe
         """
 
         if satellite in [4, 5]:
-            satellite_csv_path = self._download_l4_l5_csv()
+            satellite_csv_path = self._obtain_l4_l5_csv()
         elif satellite == 7:
-            satellite_csv_path = self._download_l7_csv()
+            satellite_csv_path = self._obtain_l7_csv()
         elif satellite in [8, 9]:
-            satellite_csv_path = self._download_l8_l9_csv()
+            satellite_csv_path = self._obtain_l8_l9_csv()
         else:
             raise ValueError("Satellite must be 4, 5, 7, 8 or 9, other satellites from landsat \
                 mission are not supported")
@@ -61,6 +63,7 @@ class UsgsProductFinder:
             output = []
         else:
             output_dataframe = pandas.DataFrame()
+        # TODO: Check if this can be threaded or multiprocessed
         for dataframe_chunk in pandas.read_csv(satellite_csv_path, chunksize=100000, low_memory=False):
             # if satellite is 7,8 or 9 convert Satellite column to str
             if satellite in [7, 8, 9]:
@@ -88,26 +91,16 @@ class UsgsProductFinder:
         else:
             return output_dataframe
 
-
-    def find_products_via_wrs_row_and_path(self, wrs_row: int, wrs_path: int, satellite: int):
-        """
-        Find products that intersect with a WRS row and path.
-        :param wrs_row:
-        :param wrs_path:
-        :param satellite:
-        :return:
-        """
-        raise NotImplementedError("This method is not implemented yet")
-
     def find_products_via_shapely_object(self, shapely_multipolygon_object: shapely.geometry.multipolygon.MultiPolygon,
                                          satellite: int,
                                          minimal_output: bool = True) -> List[Dict[str, str]] or pandas.DataFrame:
         """
         Find products that intersect with a shapely object.
-        :param shapely_multipolygon_object:
-        :param satellite:
-        :param minimal_output:
-        :return:
+        :param shapely_multipolygon_object: Shapely multipolygon object that represents the AOI
+        :param satellite: Number for the satellite [4, 5, 7, 8, 9]
+        :param minimal_output: If True, returns a list of dicts with display_id, ordering_id and landsat_product_id,
+         if False, returns a pandas dataframe from usgs csv
+        :return: List of dicts or pandas dataframe
         """
         filtered_geodataframe: geopandas.GeoDataFrame = self.geodataframe_wrs2[
             self.geodataframe_wrs2.intersects(shapely_multipolygon_object)]
@@ -117,21 +110,22 @@ class UsgsProductFinder:
         str, str]] or pandas.DataFrame:
         """
         Find products that intersect with a wkt string.
-        :param wkt:
-        :param satellite:
-        :param minimal_output:
-        :return:
+        :param wkt: WKT string that represents the AOI
+        :param satellite: Number for the satellite [4, 5, 7, 8, 9]
+        :param minimal_output: If True, returns a list of dicts with display_id, ordering_id and landsat_product_id,
+         if False, returns a pandas dataframe from usgs csv
+        :return: List of dicts or pandas dataframe
         """
         shapely_multipolygon_object = shapely.wkt.loads(wkt)
         return self.find_products_via_shapely_object(shapely_multipolygon_object, satellite, minimal_output)
 
-    def _download_file(self, url: str) -> str:
+    def _obtain_file(self, url: str) -> str:
         """
-        Download a file from specified url into the path specified in the constructor.
+        obtain a file from specified url into the path specified in the constructor.
 
-        Skips the download if the file already exists and is newer than self.max_age_of_usgs_data.
-        :param url: URL of a file to download
-        :return:  Newly downloaded file's path
+        Skips the obtain if the file already exists and is newer than self.max_age_of_usgs_data.
+        :param url: URL of a file to obtain
+        :return:  Newly obtained file's path
         """
         local_filepath = os.path.join(self.path_for_usgs_data, os.path.basename(url))
         logger.debug(f"Checking if {local_filepath} exists and is less than {self.max_age_of_usgs_data} days old")
@@ -145,41 +139,41 @@ class UsgsProductFinder:
                         )
                 )
         ):
-            logger.debug(f"Downloading CSV file from {url} into {local_filepath}")
+            logger.debug(f"obtaining CSV file from {url} into {local_filepath}")
             r = requests.get(url, stream=True)
             with open(local_filepath, "wb") as f:
                 f.write(r.content)
-            logger.debug("Download complete")
+            logger.debug("obtain complete")
         else:
             logger.debug(
-                "CSV file already exists and is less than 7 days old, not downloading again"
+                "CSV file already exists and is less than 7 days old, not obtaining again"
             )
         logger.debug(f"Returning {local_filepath}")
         return local_filepath
 
-    def _download_l4_l5_csv(self) -> str:
+    def _obtain_l4_l5_csv(self) -> str:
         """
-        Download the csv's for Landsat 4 and 5.
-        :return: New file location
+        Obtain the csv's for Landsat 4 and 5, downloading if necessary.
+        :return: FIle location
         """
         l4_l5_csv_url = "https://landsat.usgs.gov/landsat/metadata_service/bulk_metadata_files/LANDSAT_TM_C2_L2.csv.gz"
-        return self._download_file(l4_l5_csv_url)
+        return self._obtain_file(l4_l5_csv_url)
 
-    def _download_l7_csv(self):
+    def _obtain_l7_csv(self):
         """
-        Download the csv for Landsat 7.
-        :return: New file location
+        Obtain the csv for Landsat 7, downloading if necessary.
+        :return: FIle location
         """
         l7_url = "https://landsat.usgs.gov/landsat/metadata_service/bulk_metadata_files/LANDSAT_ETM_C2_L2.csv.gz"
-        return self._download_file(l7_url)
+        return self._obtain_file(l7_url)
 
-    def _download_l8_l9_csv(self):
+    def _obtain_l8_l9_csv(self):
         """
-        Download the csv's for Landsat 8 and 9.
-        :return: New file location
+        Obtain the csv's for Landsat 8 and 9, downloading if necessary.
+        :return: FIle location
         """
         l8_l9_url = "https://landsat.usgs.gov/landsat/metadata_service/bulk_metadata_files/LANDSAT_OT_C2_L2.csv.gz"
-        return self._download_file(l8_l9_url)
+        return self._obtain_file(l8_l9_url)
 
     def _load_in_wrs2(self):
         """
