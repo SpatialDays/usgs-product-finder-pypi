a61aff7106b5b1943bfa4ac033f8429471c57a50
Ivica Matic <ivica.matic@spatialdays.com>
2023-10-18 11:04:58 +0100

    feat: added ability to download files

--- usgs_product_finder/usgs_product_finder.py
+++ usgs_product_finder/usgs_product_finder.py
@@ -2,13 +2,13 @@ import logging
 
 # make a logger called "usgs_product_finder"
 logger = logging.getLogger("usgs_product_finder")
-# set its level to log all
 logger.setLevel(logging.DEBUG)
 
 import os
 import pandas
 import geopandas
 import shapely
+import requests
 
 
 class UsgsProductFinder:
@@ -24,7 +24,15 @@ class UsgsProductFinder:
         :param path_for_usgs_data:
         :param max_age_of_usgs_data:
         """
-        pass
+        logger.debug("Initializing UsgsProductFinder")
+        self.max_age_of_usgs_data = max_age_of_usgs_data
+        if path_for_usgs_data is not None:
+            if not os.path.exists(path_for_usgs_data):
+                os.makedirs(path_for_usgs_data)
+            self.path_for_usgs_data = path_for_usgs_data
+        else:
+            self.path_for_usgs_data = os.path.join(os.path.dirname(__file__), "data")
+        self._load_in_wrs2()
 
     def find_products_via_wrs_row_and_path(self, wrs_row: int, wrs_path: int, satellite: int):
         """
@@ -56,29 +64,65 @@ class UsgsProductFinder:
 
     def _download_file(self, url: str) -> str:
         """
-        Download a file from a url into the path specified in the constructor.
+        Download a file from specified url into the path specified in the constructor.
 
         Skips the download if the file already exists and is newer than self.max_age_of_usgs_data.
         :param url: URL of a file to download
         :return:  Newly downloaded file's path
         """
+        local_filepath = os.path.join(self.path_for_usgs_data, os.path.basename(url))
+        logger.debug(f"Checking if {local_filepath} exists and is less than {self.max_age_of_usgs_data} days old")
+        if not os.path.exists(local_filepath) or (
+                (
+                        os.path.exists(local_filepath)
+                        and (
+                                os.path.getmtime(local_filepath)
+                                < (pandas.Timestamp.now() - pandas.Timedelta(days=self.max_age_of_usgs_data)).timestamp()
+                        )
+                )
+        ):
+            logger.debug(f"Downloading CSV file from {url} into {local_filepath}")
+            r = requests.get(url, stream=True)
+            with open(local_filepath, "wb") as f:
+                f.write(r.content)
+            logger.debug("Download complete")
+        else:
+            logger.debug(
+                "CSV file already exists and is less than 7 days old, not downloading again"
+            )
+        logger.debug(f"Returning {local_filepath}")
+        return local_filepath
 
-    def _download_l4_l5_csv(self):
+    def _download_l4_l5_csv(self) -> str:
         """
         Download the csv's for Landsat 4 and 5.
         :return: New file location
         """
-        pass
+        l4_l5_csv_url = "https://landsat.usgs.gov/landsat/metadata_service/bulk_metadata_files/LANDSAT_TM_C2_L2.csv.gz"
+        return self._download_file(l4_l5_csv_url)
 
     def _download_l7_csv(self):
         """
         Download the csv for Landsat 7.
         :return: New file location
         """
-        pass
+        l7_url = "https://landsat.usgs.gov/landsat/metadata_service/bulk_metadata_files/LANDSAT_ETM_C2_L2.csv.gz"
+        return self._download_file(l7_url)
 
     def _download_l8_l9_csv(self):
         """
         Download the csv's for Landsat 8 and 9.
         :return: New file location
         """
+        l8_l9_url = "https://landsat.usgs.gov/landsat/metadata_service/bulk_metadata_files/LANDSAT_OT_C2_L2.csv.gz"
+        return self._download_file(l8_l9_url)
+
+    def _load_in_wrs2(self):
+        """
+        Load in the WRS2 shapefile
+        :return:
+        """
+        wrs2_filepath = os.path.join(os.path.dirname(__file__), "files", "WRS2_descending.geojson")
+        logger.debug(f"Loading in WRS2 shapefile from {wrs2_filepath}")
+        self.geodataframe_wrs2 = geopandas.read_file(wrs2_filepath)
+        logger.debug("WRS2 shapefile loaded in")
